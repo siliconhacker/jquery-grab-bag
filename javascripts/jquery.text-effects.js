b5ebe7fb303af2015a26369b4269a6a9003e4f16
michael hood <silicon_hacker@yahoo.com>
2013-05-07 23:20:06 -0500

    Update jquery.text-effects.js

--- javascripts/jquery.text-effects.js
+++ javascripts/jquery.text-effects.js
@@ -87,4 +87,16 @@
         return this;
     };
     
+    $.fn.bannerwriter = function() {
+        this.each(function() {
+            var $ele = $(this), str = $ele.text(), progress = 0;
+            $ele.text('');
+            var timer = setInterval(function() {
+                $ele.text(str.substring(0, progress++) + (progress & 1 ? '_' : ''));
+                if (progress >= str.length) clearInterval(timer);
+            }, 100);
+        });
+        return this;
+    };
+    
 })(jQuery);
